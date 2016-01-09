0163f29e078ead724357a8ffd1a24c696ec5b50b
GPointExtreme <dominik.geiger@hotmail.com>
2016-01-09 15:02:22 +0100

    ArrayList üben.

---
+++ src/PersonDemo.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class PersonDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Person> pList = new ArrayList<>();
+		
+		Person Dominik = new Person("Dominik", "Geiger");
+		
+
+	}
+
+}
